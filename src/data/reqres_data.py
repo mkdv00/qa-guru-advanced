22ae4ae06a09e35cda80ec495b55d30adab0b59c
mkdv00 <maxim.cudaew@gmail.com>
2025-12-07 22:05:29 +0300

    feat: add base session class, update requirements

--- src/data/reqres_data.py
+++ src/data/reqres_data.py
@@ -7,3 +7,11 @@ class UserReqres:
     job: str
     updated_job: list
     email: str
+
+
+user_reqres = UserReqres(
+    name='maks',
+    job='qa',
+    updated_job=['qa', 'sdet'],
+    email='maxim.cudaew@gmail.com'
+)
---
+++ src/utils/base_session.py
@@ -0,0 +1,64 @@
+import allure
+import json
+import logging
+
+from json import JSONDecodeError
+from curlify import to_curl
+from requests import Session, Response
+from typing import Callable
+
+
+def allure_logger(func) -> Callable:
+    def wrapper(*args, **kwargs) -> Response:
+        method, url = args[1], args[2]
+
+        with allure.step(f'{method} {url}'):
+            response: Response = func(*args, **kwargs)
+
+            allure.attach(
+                body=to_curl(response.request).encode('utf8'),
+                name=f'Request: {response.status_code}'
+            )
+
+            try:
+                allure.attach(
+                    body=json.dumps(response.json(), indent=4),
+                    name=f'Response: {response.status_code}',
+                    attachment_type=allure.attachment_type.JSON,
+                    extension='.json'
+                )
+            except JSONDecodeError:
+                allure.attach(
+                    body=response.text,
+                    name=f'Response: {response.status_code}',
+                    attachment_type=allure.attachment_type.TEXT,
+                    extension='.txt'
+                )
+
+            return response
+
+    return wrapper
+
+
+def log_request(func) -> Callable:
+    def wrapper(*args, **kwargs) -> Response:
+        response: Response = func(*args, **kwargs)
+        logging.info(f'- code: {response.status_code} - {to_curl(response.request)}')
+        return response
+
+    return wrapper
+
+
+class BaseSession(Session):
+    def __init__(self, base_url: str) -> None:
+        super(BaseSession, self).__init__()
+        self.base_url = base_url
+
+    @allure_logger
+    @log_request
+    def request(self, method: str, endpoint: str, **kwargs) -> Response:
+        response = super().request(
+            method=method,
+            url=self.base_url + endpoint, **kwargs
+        )
+        return response
