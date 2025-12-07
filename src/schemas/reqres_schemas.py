b2b2fd8ff173d34ae671bad9c14618f0eca1cba9
mkdv00 <maxim.cudaew@gmail.com>
2025-12-07 22:13:13 +0300

    feat: add base chemas

---
+++ src/schemas/reqres_schemas.py
@@ -0,0 +1,14 @@
+from voluptuous import PREVENT_EXTRA, Schema, Optional
+
+
+user = Schema(
+    {
+        'id': int,
+        'email': str,
+        'first_name': str,
+        'last_name': str,
+        'avatar': str
+    },
+    extra=PREVENT_EXTRA,
+    required=True
+)
